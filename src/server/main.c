4d315527982b875d0ba319ad0f87e9a016232515
Philippe Loctaux <p@philippeloctaux.com>
2020-05-19 21:28:25 +0200

    added -help

--- src/server/main.c
+++ src/server/main.c
@@ -6,9 +6,20 @@
 */
 
 #include <stdio.h> /* for printf */
+#include <string.h> /* for strcmp */
 
-int main(void)
+static int display_usage(const char *bin, const int ret)
 {
-    printf("server main\n");
+    printf("USAGE: %s port\n\n", bin);
+    printf("\t port is the port number on which the server socket listens.\n");
+    return ret;
+}
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+        return display_usage(av[0], 84);
+    if (strcmp(av[1], "-help") == 0)
+        return display_usage(av[0], 0);
     return 0;
 }
