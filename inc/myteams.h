00f536e2446826e53abb03f87155e6b908eaa6df
Philippe Loctaux <p@philippeloctaux.com>
2020-05-21 22:44:22 +0200

    added myteams.h

---
+++ inc/myteams.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2020
+** epitech-myteams
+** File description:
+** myteams
+*/
+
+#ifndef MYTEAMS_H_
+#define MYTEAMS_H_
+
+#define MAX_NAME_LENGTH 32
+#define MAX_DESCRIPTION_LENGTH 255
+#define MAX_BODY_LENGTH 512
+
+#endif /* !MYTEAMS_H_ */
