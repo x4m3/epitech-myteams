60af0db1a1b1cfd12f10b8d5959a2bc3061a33f2
Philippe Loctaux <p@philippeloctaux.com>
2020-05-21 22:46:18 +0200

    myteams.h: ugh

--- inc/myteams.h
+++ inc/myteams.h
@@ -8,8 +8,8 @@
 #ifndef MYTEAMS_H_
 #define MYTEAMS_H_
 
-#define MAX_NAME_LENGTH 32
+#define MAX_NAME_LENGTH        32
 #define MAX_DESCRIPTION_LENGTH 255
-#define MAX_BODY_LENGTH 512
+#define MAX_BODY_LENGTH        512
 
 #endif /* !MYTEAMS_H_ */
