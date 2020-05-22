78c58a7c717b276bb0e4bd7316c266953ed75ac8
Philippe Loctaux <p@philippeloctaux.com>
2020-05-23 00:06:41 +0200

    added client_handle.c

---
+++ src/server/client_handle.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2020
+** epitech-myteams
+** File description:
+** client_handle
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "server.h"
+
+bool client_handle(net_user_t *net_user)
+{
+    char *input = NULL;
+    size_t input_len = 0;
+    ssize_t getline_ret = getline(&input, &input_len, net_user->input);
+
+    if (getline_ret == -1)
+        return false;
+    printf("got input from client: [%s]\n", input);
+    free(input);
+    return true;
+}
