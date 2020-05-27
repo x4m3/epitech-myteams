56ab30208bbfdfbf7ccc8842759559d6c64ec458
theo boscher <theo.boscher@epitech.eu>
2020-05-28 01:11:37 +0200

    add function to add thread

---
+++ src/server/thread/add_thread.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2019
+** name of the project
+** File description:
+** foo
+*/
+
+#include "server.h"
+
+thread_t *add_channel(char author[MAX_NAME_LENGTH],
+    char title[MAX_DESCRIPTION_LENGTH], char message[MAX_BODY_LENGTH],
+    channel_t *channel)
+{
+    uuid_t new_uuid_bin;
+
+    if ((channel->list_of_thread = malloc(sizeof(team_t))) == NULL)
+        return NULL;
+    uuid_generate_random(new_uuid_bin);
+    uuid_unparse(new_uuid_bin, channel->list_of_thread->thread_uuid);
+    strcpy(channel->list_of_thread->title, title);
+    strcpy(channel->list_of_thread->message, message);
+    strcpy(channel->list_of_thread->author, author);
+    TAILQ_INSERT_HEAD(
+        &channel->thread_head, channel->list_of_thread, next_thread);
+    printf("add new channel: [%s] [%s]\n", channel->list_of_thread->title,
+        channel->list_of_thread->message);
+    return channel->list_of_thread;
+}
