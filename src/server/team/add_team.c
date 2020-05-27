7092d6947761e2e3666e927ad363a6b4d14f8016
theo boscher <theo.boscher@epitech.eu>
2020-05-28 00:57:45 +0200

    add function to add a team

--- src/server/team/add_team.c
+++ src/server/team/add_team.c
@@ -7,20 +7,19 @@
 
 #include "server.h"
 
-team_t *add_team(
-    char name[MAX_NAME_LENGTH], char description[MAX_DESCRIPTION_LENGTH], my_teams_t *myTeams)
+team_t *add_team(char name[MAX_NAME_LENGTH],
+    char description[MAX_DESCRIPTION_LENGTH], my_teams_t *myTeams)
 {
-    team_t *new_team;
     uuid_t new_uuid_bin;
 
-    if ((new_team = malloc(sizeof(team_t))) == NULL)
+    if ((myTeams->list_of_team = malloc(sizeof(team_t))) == NULL)
         return NULL;
     uuid_generate_random(new_uuid_bin);
-    uuid_unparse(new_uuid_bin, new_team->team_uuid);
-    strcpy(new_team->description, description);
-    strcpy(new_team->name, name);
-    TAILQ_INSERT_TAIL(&myTeams->team_head, new_team, next_team);
-    printf(
-        "add new user: [%s] [%s]\n", new_team->name, new_team->description);
-    return new_team;
+    uuid_unparse(new_uuid_bin, myTeams->list_of_team->team_uuid);
+    strcpy(myTeams->list_of_team->description, description);
+    strcpy(myTeams->list_of_team->name, name);
+    TAILQ_INSERT_HEAD(&myTeams->team_head, myTeams->list_of_team, next_team);
+    printf("add new team: [%s] [%s]\n", myTeams->list_of_team->name,
+        myTeams->list_of_team->description);
+    return myTeams->list_of_team;
 }
