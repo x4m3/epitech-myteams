b738d7ebebfc9f914c7968cd18b7cbdf9bb7d097
theo boscher <theo.boscher@epitech.eu>
2020-05-30 17:40:14 +0200

    user cmd : remove remove quotes

--- src/server/cmd/user.c
+++ src/server/cmd/user.c
@@ -11,13 +11,14 @@ void cmd_user(net_user_t *user, char **args)
 {
     bool found = false;
     my_teams_t *global_teams = get_global_teams(NULL);
-    char *param_no_quotes = NULL;
+    char *user_uuid = args[1];
 
-    param_no_quotes = remove_quotes(args[1]);
+    if (check_input_args(1, args, user->socket_fd) == false)
+        return;
     TAILQ_FOREACH(
         global_teams->users, &global_teams->user_info_head, next_users)
     {
-        if (strcmp(global_teams->users->username, param_no_quotes) == 0) {
+        if (strcmp(global_teams->users->username, user_uuid) == 0) {
             client_response(user->socket_fd, global_teams->users->username);
             client_response(user->socket_fd, global_teams->users->user_uuid);
             (global_teams->users->online == true)
