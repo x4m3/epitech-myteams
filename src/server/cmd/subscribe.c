d4e5c124ccc937703b0a2e9b08f3a8e543596d2e
theo boscher <theo.boscher@epitech.eu>
2020-05-30 17:36:45 +0200

    subscribe cmd : remove remove quotes

--- src/server/cmd/subscribe.c
+++ src/server/cmd/subscribe.c
@@ -20,15 +20,11 @@ team_t *is_team_exist(my_teams_t *myTeams, char uuid_send[UUID_STR_LEN])
 void cmd_subscribe(net_user_t *user, char **args)
 {
     my_teams_t *global_teams = get_global_teams(NULL);
-    char *param_no_quotes = NULL;
+    char *team_uuid = args[1];
 
-    param_no_quotes = remove_quotes(args[1]);
-    if (is_team_exist(global_teams, param_no_quotes) != NULL) {
-        global_teams->list_of_team =
-            is_team_exist(global_teams, param_no_quotes);
-        printf("%p\n", &global_teams->list_of_team->my_teams_uuid_head);
-        add_team_uuid(global_teams->list_of_team, user->user->user_uuid);
-        client_response(user->socket_fd, "welcome to the team");
-    } else
-        client_response(user->socket_fd, "this uuid_team isn't exist");
+    if (is_team_exist(global_teams, team_uuid) == NULL)
+        return client_response(user->socket_fd, "this uuid_team isn't exist");
+    global_teams->list_of_team = is_team_exist(global_teams, team_uuid);
+    add_team_uuid(global_teams->list_of_team, user->user->user_uuid);
+    client_response(user->socket_fd, "welcome to the team");
 }
