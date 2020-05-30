e0f8aa1887b834d185582997bd020d0ab931745b
Philippe Loctaux <p@philippeloctaux.com>
2020-05-31 00:19:27 +0200

    write_users.c

--- src/server/database/write/write_users.c
+++ src/server/database/write/write_users.c
@@ -2,13 +2,13 @@
 ** EPITECH PROJECT, 2020
 ** epitech-myteams
 ** File description:
-** write_user
+** write_users
 */
 
 #include "database.h"
 #include "server.h"
 
-bool write_user(user_info_t *user)
+static bool write_user(user_info_t *user)
 {
     FILE *output = NULL;
     char *filename = construct_filename(user->user_uuid, USER);
@@ -22,5 +22,15 @@ bool write_user(user_info_t *user)
         return false;
     }
     printf("filepath: [%s]\n", filepath);
+    free(filepath);
+    return true;
+}
+
+bool write_users(my_teams_t *my_teams)
+{
+    TAILQ_FOREACH(my_teams->users, &my_teams->user_info_head, next_users)
+    {
+        write_user(my_teams->users);
+    }
     return true;
 }
