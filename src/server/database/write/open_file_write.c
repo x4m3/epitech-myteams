e910bd26df90a5f5526f18400841e7d61fd7bbae
Philippe Loctaux <p@philippeloctaux.com>
2020-05-30 23:35:59 +0200

    added open_file_write

---
+++ src/server/database/write/open_file_write.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2020
+** epitech-myteams
+** File description:
+** open_file_write
+*/
+
+#include <stdio.h>
+
+FILE *open_file_write(const char *file_path)
+{
+    FILE *file = fopen(file_path, "w");
+
+    if (file == NULL) {
+        printf("[database]: failed to open [%s] for writing.\n", file_path);
+        return NULL;
+    }
+    return file;
+}
