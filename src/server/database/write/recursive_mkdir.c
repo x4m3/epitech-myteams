7dd6d162eb581cd594571a3f6bfd32b16f55c8b3
Philippe Loctaux <p@philippeloctaux.com>
2020-05-31 00:19:14 +0200

    mkdir

---
+++ src/server/database/write/recursive_mkdir.c
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2020
+** epitech-myteams
+** File description:
+** recursive_mkdir
+*/
+
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void recursive_mkdir(const char *dir)
+{
+    char tmp[256];
+    char *p = NULL;
+    size_t len;
+
+    snprintf(tmp, sizeof(tmp), "%s", dir);
+    len = strlen(tmp);
+    if (tmp[len - 1] == '/')
+        tmp[len - 1] = 0;
+    for (p = tmp + 1; *p; p++)
+        if (*p == '/') {
+            *p = 0;
+            mkdir(tmp, 0775);
+            *p = '/';
+        }
+    mkdir(tmp, 0775);
+}
