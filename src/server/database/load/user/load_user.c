70ede042a6bd2d4c722cffe03999281c7ec832b2
Philippe Loctaux <p@philippeloctaux.com>
2020-05-31 03:12:35 +0200

    load_user.c: opening file

--- src/server/database/load/user/load_user.c
+++ src/server/database/load/user/load_user.c
@@ -10,6 +10,26 @@
 #include <stdio.h>
 #include "database.h"
 
+static void load_user_data(FILE *input, my_teams_t *my_teams)
+{
+}
+
+static bool load_user(my_teams_t *my_teams, char *filename)
+{
+    char *filepath = construct_filepath(DB_USERS_PATH, filename);
+    FILE *input = NULL;
+
+    if (filepath == NULL)
+        return false;
+    input = open_file_read(filepath);
+    if (input == NULL)
+        return false;
+    load_user_data(input, my_teams);
+    free(filepath);
+    fclose(input);
+    return true;
+}
+
 bool load_users(my_teams_t *my_teams)
 {
     struct dirent *dir_dirent = NULL;
@@ -20,7 +40,7 @@ bool load_users(my_teams_t *my_teams)
     while ((dir_dirent = readdir(directory)) != NULL) {
         if (dir_dirent->d_type != DT_REG)
             continue;
-        printf("%s\n", dir_dirent->d_name);
+        load_user(my_teams, dir_dirent->d_name);
     }
     closedir(directory);
     return true;
