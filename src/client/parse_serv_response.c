9d5812891326af2130d8874061b950af56c08042
Kenzer <kenny.buquen@epitech.eu>
2020-05-31 23:26:40 +0200

    add USER command on client

--- src/client/parse_serv_response.c
+++ src/client/parse_serv_response.c
@@ -10,22 +10,22 @@
 static void attribute_cmd(command_t *cmd)
 {
     if (!cmd->cmd)
-        return;
+        return; 
     cmd->c = NONE;
     cmd->c = (strncmp(cmd->cmd, "/login", 6) == 0) ? (LOGIN) : (cmd->c);
     cmd->c = (strncmp(cmd->cmd, "/logout", 7) == 0) ? (LOGOUT) : (cmd->c);
     cmd->c = (strncmp(cmd->cmd, "/help", 5) == 0) ? (HELP) : (cmd->c);
-    cmd->c = (strncmp(cmd->cmd, "/users", 6) == 0) ? (USERS) : (cmd->c);
-    cmd->c = (strncmp(cmd->cmd, "/user", 5) == 0) ? (USER) : (cmd->c);
+    cmd->c = (strcmp(cmd->cmd, "/users") == 0) ? (USERS) : (cmd->c);
+    cmd->c = (strcmp(cmd->cmd, "/user") == 0) ? (USER) : (cmd->c);
     cmd->c = (strncmp(cmd->cmd, "/send", 5) == 0) ? (SEND) : (cmd->c);
     cmd->c = (strncmp(cmd->cmd, "/messages", 9) == 0) ? (MESSAGES) : (cmd->c);
     cmd->c =
-        (strncmp(cmd->cmd, "/subscribe", 10) == 0) ? (SUBSCRIBE) : (cmd->c);
+        (strcmp(cmd->cmd, "/subscribe") == 0) ? (SUBSCRIBE) : (cmd->c);
     cmd->c =
-        (strncmp(cmd->cmd, "/subscribed", 11) == 0) ? (SUBSCRIBED) : (cmd->c);
+        (strcmp(cmd->cmd, "/subscribed") == 0) ? (SUBSCRIBED) : (cmd->c);
     cmd->c = (strncmp(cmd->cmd, "/unsubscribe", 12) == 0) ? (UNSUBSCRIBE)
                                                           : (cmd->c);
-    cmd->c = (strncmp(cmd->cmd, "/use", 4) == 0) ? (USE) : (cmd->c);
+    cmd->c = (strcmp(cmd->cmd, "/use") == 0) ? (USE) : (cmd->c);
     cmd->c = (strncmp(cmd->cmd, "/create", 7) == 0) ? (CREATE) : (cmd->c);
     cmd->c = (strncmp(cmd->cmd, "/list", 5) == 0) ? (LIST) : (cmd->c);
     cmd->c = (strncmp(cmd->cmd, "/info", 5) == 0) ? (INFO) : (cmd->c);
