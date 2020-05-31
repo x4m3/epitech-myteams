96eeabc04805a90aeceadbc5b388a59b04029d6d
theo boscher <theo.boscher@epitech.eu>
2020-05-31 19:45:41 +0200

    remove all leak memory

--- src/server/direct_message/delete_direct_message.c
+++ src/server/direct_message/delete_direct_message.c
@@ -13,6 +13,8 @@ void delete_direct_message(my_teams_t *my_team)
         my_team->list_of_direct_message = TAILQ_FIRST(&my_team->direct_message_head);
         TAILQ_REMOVE(
             &my_team->direct_message_head, my_team->direct_message_head.tqh_first, next_direct_message);
+        if (my_team->list_of_direct_message != NULL)
+            delete_conversation(my_team->list_of_direct_message);
         free(my_team->list_of_direct_message);
     }
 }
