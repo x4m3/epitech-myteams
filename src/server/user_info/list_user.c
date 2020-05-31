95f65d70a41e8f14d4d16128e41eb04f3da83269
theo boscher <theo.boscher@epitech.eu>
2020-05-31 23:10:23 +0200

    coding style issues

--- src/server/user_info/list_user.c
+++ src/server/user_info/list_user.c
@@ -13,8 +13,8 @@ void list_user(my_teams_t *my_teams, int socket_user)
 
     TAILQ_FOREACH(my_teams->users, &my_teams->user_info_head, next_users)
     {
-        param_to_send_cli = concat_args_to_cli(
-            my_teams->users->user_uuid, my_teams->users->username,
+        param_to_send_cli = concat_args_to_cli(my_teams->users->user_uuid,
+            my_teams->users->username,
             (my_teams->users->online == true) ? "0" : "1", NULL);
         client_response(socket_user, param_to_send_cli);
         free(param_to_send_cli);
