9a25e25cea48d42d1d73c31276b9defbff1acb7d
theo boscher <theo.boscher@epitech.eu>
2020-05-31 03:34:23 +0200

    find instance

--- src/server/instance/find_instance.c
+++ src/server/instance/find_instance.c
@@ -4,3 +4,15 @@
 ** File description:
 ** foo
 */
+
+#include "server.h"
+
+instance_t *find_instance(user_info_t *user, int socket)
+{
+    TAILQ_FOREACH(user->list_of_instance, &user->instance_head, next_instance)
+    {
+        if (user->list_of_instance->socket_fd == socket)
+            return user->list_of_instance;
+    }
+    return NULL;
+}
